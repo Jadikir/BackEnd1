40d64d49135b44071342ea8f4c6dba7a1516c75f
Jadikir <113051399+Jadikir@users.noreply.github.com>
2023-03-27 18:11:17 +0300

    Update modelController.js

--- server/controllers/modelController.js
+++ server/controllers/modelController.js
@@ -1,35 +1,15 @@
-const uuid = require('uuid')
-const path = require('path')
-const mimeE=require('mime')
-const {setOfModel} = require('../models/models')
+const {Zakaz,User} = require('../models/models')
 const  ApiError=require('../error/ApiError')
-const myArray = ["image/jpeg","image/png"];
-const myArray1 = ["application/rar","application/zip","application/vnd.rar","application/x-7z-compressed"];
 const fs = require('fs');
 class modelController {
 
         async create(req, res, next) {
                 try {
-                        const {name, author, price, typeId} = req.body
-                        const {render} = req.files
-                        const {file_model} = req.files
-                        if (myArray.includes(mimeE.getType(render.name)) && myArray1.includes(mimeE.getType(file_model.name))) {
-                                let filename = uuid.v4() + ".jpg"
-                                let file_filename = uuid.v4() + ".zip"
-                                await render.mv(path.resolve(__dirname, '..', 'static', filename))
-                                await file_model.mv(path.resolve(__dirname, '..', 'static', file_filename))
-                                const oneModel = await setOfModel.create({
-                                        name,
-                                        author,
-                                        file_model: file_filename,
-                                        price,
-                                        render: filename,
-                                        typeId
-                                })
-                                return res.json(oneModel)
-                        } else {
-                                next(ApiError.badRequest("Bad files"))
-                        }
+
+                        const {name,price,description} = req.body
+                        var id_Zakazchika = User.id
+                        const zakaz = await Zakaz.create({name,id_Zakazchika,price,description})
+                        return res.json(zakaz)
                 } catch (e) {
                         next(ApiError.badRequest("Something wrong"))
                 }
@@ -61,17 +41,8 @@ class modelController {
         }
         async del(req,res,next) {
                 try{
-                        let temp = req.body
-                        let temp2 = JSON.stringify(await setOfModel.findAll( {where:{name:'Dodster3'},attributes: ['render'],raw: true}))
-                        let temp3 = JSON.stringify(await setOfModel.findAll( {where:{name:'Dodster3'},attributes: ['file_model'],raw: true}))
-                        console.log(temp2,temp3)
-                        temp2=temp2.slice(12,temp2.length-3)
-                        temp3=temp3.slice(16,temp3.length-3)
-                        console.log(path.resolve(__dirname, '..', 'static', temp2))
-                        fs.unlinkSync(path.resolve(__dirname, '..', 'static', temp2))
-                        fs.unlinkSync(path.resolve(__dirname, '..', 'static', temp3))
-                        await setOfModel.destroy( {
-                                where:{name:'Dodster3'}
+                        await Zakaz.destroy( {
+                                where:{id:this.getAll()}
                         })
                         return res.json()
                         }
