f32e2c89c5558b6cb7a2ee2887a29fd26a26e294
Jadikir <113051399+Jadikir@users.noreply.github.com>
2023-05-04 21:42:58 +0300

    цукцук

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import logo from './logo.svg';
-import './App.css';
 import React from 'react';
 import {BrowserRouter} from "react-router-dom";
 import AppRouter from "./components/AppRouter";
--- client/src/components/ZakazItem.js
+++ client/src/components/ZakazItem.js
@@ -1,28 +1,44 @@
-import React, {useContext} from 'react';
-import {Accordion, Button} from "react-bootstrap";
-import {useLocation, useNavigate} from 'react-router-dom'
-import {LOGIN_ROUTE, ZAKAZPAGE_ROUTE} from "../utils/consts";
+import React, {useContext, useState} from 'react';
+import {Accordion, Button,Modal} from "react-bootstrap";
+import {useNavigate} from 'react-router-dom'
+import { ZAKAZPAGE_ROUTE} from "../utils/consts";
 import {Context} from "../index";
-
+import Zakaziki from "../Zakaznai/Zakaziki";
 
 const ZakazItem = ({Zakaziki}) => {
     const navigate = useNavigate()
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     const {user} = useContext(Context)
-    const location = useLocation()
+
     return (
         <Accordion>
             <Accordion.Item eventKey="0">
-                <Accordion.Header>Первое слово на татарском</Accordion.Header>
+                <Accordion.Header>{Zakaziki.name}</Accordion.Header>
                 <Accordion.Body className="d-flex align-items-center">
                     <div className="flex-grow-1">
                         Что-то на татарском
                     </div>
-                    <div className="ml-auto">
-                        {user.isAuth &&
-                        <Button variant="danger" as="input" type="button" value="ВЫРЕЗАТЬ ТВАРЬ" onClick={() => navigate(ZAKAZPAGE_ROUTE + '/' + Zakaziki.id)}/>}
-                        <Button variant="info" as="input" type="button" value="Нажми если интересно" onClick={() => navigate(ZAKAZPAGE_ROUTE + '/' + Zakaziki.id)}/>
-
-                    </div>
+                    {(user.isAuth&&
+                        <Button variant="danger" variant="outline-danger" onClick={handleShow}>
+                            ВЫРЕЗАТЬ ТВАРЬ
+                        </Button>)}
+                        <Modal show={show} onHide={handleClose}>
+                            <Modal.Header closeButton>
+                                <Modal.Title>Modal heading</Modal.Title>
+                            </Modal.Header>
+                            <Modal.Body>Этот заказ точно нарушает политику капибар?</Modal.Body>
+                            <Modal.Footer>
+                                <Button variant="secondary" onClick={handleClose}>
+                                    НЕт, миссклик
+                                </Button>
+                                <Button variant="primary" onClick={handleClose}>
+                                    Да,вырезать
+                                </Button>
+                            </Modal.Footer>
+                        </Modal>
+                        <Button className="my-2" variant="info" as="input" type="button" value="Нажми если интересно" onClick={() => navigate(ZAKAZPAGE_ROUTE + '/' + Zakaziki.id)}/>
                 </Accordion.Body>
             </Accordion.Item>
         </Accordion>
